6c70f1dda68600d1085a88b4156470d5c7538ade
ryecatcher <abhik.rk@gmail.com>
2011-02-12 10:50:57 -0800

    Finishd first cut of the User model

--- Gemfile~
+++ Gemfile~
@@ -37,5 +37,6 @@ end
 group :test do
   gem 'rspec'
   gem 'webrat'
+  gem 'spork'
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 SampleAppRails3::Application.routes.draw do
-  get "users/new"
+  resources :users
+
   match '/signup',  :to => 'users#new'
 
   match '/contact', :to => 'pages#contact'
